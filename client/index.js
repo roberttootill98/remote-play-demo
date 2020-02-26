6b9a8e6121772035e59a2c1e2d81fab563e64880
Rob T <Robert.Tootill98@gmail.com>
2020-02-26 11:16:41 +0000

    basic server structure

---
+++ client/index.js
@@ -0,0 +1,5 @@
+'use strict'
+
+function demoFunc() {
+  console.log("hi");
+}
---
+++ server/server.js
@@ -0,0 +1,15 @@
+'use strict'
+
+const express = require('express');
+const app = express();
+//const db = require('./ModelSQL.js');
+
+app.get('/api/item', getItem);
+
+async function getItem(req, res) {
+    res.json({'item': 0});
+}
+
+app.listen(8080);
+
+app.use('/', express.static('../client', {'extensions': ['html']}));
